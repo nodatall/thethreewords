f2afb862b371af46b2a038f5fa3a229643a0ec5a
The Three Words <sswierkosz@gmail.com>
2024-02-28 21:37:20 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,10 +2,15 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
-      </footer>
-    </>
-  )
+    <footer style={{
+      width: '100%',
+      padding: '20px',
+      textAlign: 'center',
+      color: 'white',
+    }}>
+      <a href="https://twitter.com/7heThreeWords" target="_blank" rel="noopener noreferrer" style={{color: 'white', textDecoration: 'none'}}>
+        Find us on Twitter
+      </a>
+    </footer>
+  );
 }
